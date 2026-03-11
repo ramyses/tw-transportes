b7714022275cbc60d6539c5756ee8fa31afb438c
ramyses <lukasramysesleao@gmail.com>
2026-03-11 17:17:37 -0400

    criando pagina de rastreamento

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Http\Controllers\RastreamentoController;
 use App\Http\Controllers\HomeController;  
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
+Route::get('/rastreamento', RastreamentoController::class);
