bef62e806e5f70b958fbc1c2e5a4b418696d54bb
ramyses <lukasramysesleao@gmail.com>
2026-03-13 10:19:48 -0400

    tornando a pagina historico dinamica

--- app/Http/Controllers/HistoricoController.php
+++ app/Http/Controllers/HistoricoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cliente;
 use Illuminate\Http\Request;
 
 class HistoricoController extends Controller
@@ -12,6 +13,17 @@ class HistoricoController extends Controller
     public function __invoke(Request $request)
     {
         //
-        return view('frete.historico');
+        $telefone = $request->input('telefone',"");
+        $cliente = Cliente::where('telefone',$telefone)
+                                ->with('enviados', 'recebidos')
+                                ->first();
+
+
+        if(! $cliente){
+            return redirect()->back()->with('error', 'Cliente não encontrado');
+        }
+        return view('frete.historico', [
+            'cliente' => $cliente,
+        ]);
     }
 }
