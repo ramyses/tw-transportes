d1c450e58bca989d91728edf65ddb88161921d24
ramyses <lukasramysesleao@gmail.com>
2026-03-13 09:24:35 -0400

    criando busca dinamica

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
-Route::get('/rastreamento', RastreamentoController::class);
+Route::get('/rastreamento', RastreamentoController::class)->name('frete.rastreamento');
